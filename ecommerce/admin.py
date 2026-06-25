44f5568a01fd24657c8b2df26d34589b4b11e856
Apurva Patel <patelappu1495@gmail.com>
2026-06-25 21:03:25 +0530

    add media url in admin.py

--- ecommerce/admin.py
+++ ecommerce/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import (
+    Brand,
+    Category,
+    Product,
+    ProductImage
+)
+
+
+@admin.register(Brand)
+class BrandAdmin(admin.ModelAdmin):
+
+    list_display = [
+        "id",
+        "name",
+        "logo"
+    ]
+
+
+admin.site.register(Category)
+admin.site.register(Product)
+admin.site.register(ProductImage)
